297ae0124c64283d69fc3651501a9d8caf08c487
OdaShow <shota.oda+github@bizreach.co.jp>
2018-04-08 13:41:02 +0900

    re-eject

--- android/app/src/main/java/org/carbon/rocketty/generated/ExponentBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package org.carbon.rocketty.generated;
-
-// This file is auto-generated. Please don't rename!
-public class ExponentBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "";
-
-}
